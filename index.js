f24dff48f88a375b09b5c676c019408f88860d7c
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-01-29 11:14:04 +0400

    clear input fields, edit page location, save task function

--- index.js
+++ index.js
@@ -20,4 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
     clearInputFields();
     loadTasks();
   }
+
+  function clearInputFields() {
+    document.getElementById('taskName').value = '';
+    document.getElementById('taskDescription').value = '';
+  }
+  
+  function editTask(taskId) {
+    window.location.href = `http://127.0.0.1:5500/edit.html=${taskId}`;
+  }
+  function saveTask(task) {
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  
   
