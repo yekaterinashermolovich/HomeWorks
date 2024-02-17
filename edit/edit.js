23deca8dc91aeede17babdf2af7a93e33f413046
yekaterinashermolovichя <k.shermolovich@hotmail.comm>
2024-02-17 18:51:34 +0400

    corrected copy&paste all files

--- edit/edit.js
+++ edit/edit.js
@@ -1,19 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
-    const taskId = new URL(window.location.href).searchParams.get("id");
-    
-    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-   
-    const currentTaskIndex = tasks.findIndex((task) => task.id === taskId);
-   
-    const currentTask = tasks[currentTaskIndex];
-  
-   
+  const taskId = new URL(window.location.href).searchParams.get("id");
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  const currentTaskIndex = tasks.findIndex((task) => task.id === taskId);
+  const currentTask = tasks[currentTaskIndex];
+
   const editTaskForm = document.getElementById("editTaskForm");
   const taskNameInput = document.getElementById("taskName");
   const taskDescriptionInput = document.getElementById("taskDescription");
 
- 
   if (!currentTask) {
     editTaskForm.innerHTML = "Task not found";
     return;
@@ -22,22 +16,29 @@ document.addEventListener("DOMContentLoaded", () => {
   taskNameInput.value = currentTask.name;
   taskDescriptionInput.value = currentTask.description;
 
-  
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    
     const formData = new FormData(editTaskForm);
     const formProps = Object.fromEntries(formData);
 
-   
     tasks[currentTaskIndex] = {
       ...currentTask,
       name: formProps.taskName,
       description: formProps.taskDescription,
     };
 
-   
-      localStorage.setItem("tasks", JSON.stringify(tasks)); 
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
-}});
+    console.log("Updated tasks:", tasks);
+  };
+
+  editTaskForm.addEventListener("submit", handleFormSubmit);
+
+  const backButton = document.getElementById("backButton");
+  if (backButton) {
+    backButton.addEventListener("click", () => {
+      history.back();
+    });
+  }
+});
--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', () => {
-    loadTasks();
-  });
-  
-  function addTask() {
-    const taskName = document.getElementById('taskName').value;
-    const taskDescription = document.getElementById('taskDescription').value;
-    const taskId = generateUniqueId();
-    const taskDate = new Date().toLocaleDateString();
-    const task = {
-      id: taskId,
-      name: taskName,
-      description: taskDescription,
-      date: taskDate,
-      completed: false,
-      edited: false,
-    };
+document.addEventListener("DOMContentLoaded", () => {
+  loadTasks();
+});
 
-    let taskNameValue = document.getElementById("taskName").value;
-    let taskDescriptionValue = document.getElementById("taskDescription").value;
+function addTask() {
+  const taskName = document.getElementById("taskName").value;
+  const taskDescription = document.getElementById("taskDescription").value;
 
-    let taskNameRegex = /^[a-zA-Z0-9\s]+$/;
-    let taskDescriptionRegex = /^[a-zA-Z0-9\s]+$/;
+  const taskId = generateUniqueId();
+  const taskDate = new Date().toLocaleDateString();
+  const task = {
+    id: taskId,
+    name: taskName,
+    description: taskDescription,
+    date: taskDate,
+    completed: false, 
+    edited: false,
+  };
 
-  
+  let taskNameValue = document.getElementById("taskName").value;
+  let taskDescriptionValue = document.getElementById("taskDescription").value;
+
+  let taskNameRegex = /^[a-zA-Z0-9\s]+$/;
+  let taskDescriptionRegex = /^[a-zA-Z0-9\s]+$/;
+
   if (!taskNameRegex.test(taskNameValue)) {
     alert("Not correct name");
     return false;
@@ -32,115 +32,119 @@ document.addEventListener('DOMContentLoaded', () => {
     alert("Not correct description");
     return false;
   }
-  
-    saveTask(task);
-    clearInputFields();
-    loadTasks();
+
+  saveTask(task);
+  clearInputFields();
+  loadTasks();
+}
+
+function clearInputFields() {
+  document.getElementById("taskName").value = "";
+  document.getElementById("taskDescription").value = "";
+}
+
+function editTask(taskId) {
+  const editUrl = `/edit/edit.html?id=${taskId}`;
+  window.location.assign(editUrl);
+}
+
+function saveTask(task) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.unshift(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function loadTasks(
+  isSortByDate = false,
+  isSortByTitle = false,
+  filterCompleted = "all"
+) {
+  const incompletedTasksDiv = document.getElementById("incompletedTasks");
+  const completedTasksDiv = document.getElementById("completedTasks");
+  incompletedTasksDiv.innerHTML = "";
+  completedTasksDiv.innerHTML = "";
+
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  if (isSortByDate) {
+    tasks.sort((a, b) => new Date(b.date) - new Date(a.date));
   }
 
-  function clearInputFields() {
-    document.getElementById('taskName').value = '';
-    document.getElementById('taskDescription').value = '';
-  }
-  
-  function editTask(taskId) {
-    window.location.href = `http://127.0.0.1:5500/edit/edit.html?id=${taskId}`;
+  if (isSortByTitle) {
+    tasks.sort((a, b) => a.name.localeCompare(b.name));
   }
 
-   function saveTask(task) {
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    tasks.unshift(task);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+  if (filterCompleted === "completed") {
+    tasks = tasks.filter((task) => task.completed);
+  } else if (filterCompleted === "incompleted") {
+    tasks = tasks.filter((task) => !task.completed);
   }
 
-  function loadTasks(
-    sortByDate = false,
-    sortByTitle = false,
-    filterCompleted = "all"
-  ) {
-    const incompletedTasksDiv = document.getElementById('incompletedTasks');
-    const completedTasksDiv = document.getElementById('completedTasks');
-    incompletedTasksDiv.innerHTML = '';
-    completedTasksDiv.innerHTML = '';
-  
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.forEach((task) => {
+    const taskDiv = document.createElement("div");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = task.completed;
+    checkbox.addEventListener("change", () => toggleTaskCompletion(task.id));
 
-    if (sortByDate) {
-      tasks.sort((a, b) => new Date(b.date) - new Date(a.date));
-    }
-  
-    if (sortByTitle) {
-      tasks.sort((a, b) => a.name.localeCompare(b.name));
-    }
-  
-    
-    if (filterCompleted === "completed") {
-      tasks = tasks.filter((task) => task.completed);
-    } else if (filterCompleted === "incompleted") {
-      tasks = tasks.filter((task) => !task.completed);
-    }
-  
-    tasks.forEach(task => {
-      const taskDiv = document.createElement('div');
-      const checkbox = document.createElement('input');
-      checkbox.type = 'checkbox';
-      checkbox.checked = task.completed;
-      console.log(task.completed)
-      checkbox.addEventListener('change', () => toggleTaskCompletion(task.id));
-      
-      const taskInfo = document.createElement('div');
-      taskInfo.innerHTML = `
+    const taskNameDiv = document.createElement("div");
+    taskNameDiv.innerText = task.name;
+    taskNameDiv.style.cursor = "pointer"; 
+    taskNameDiv.addEventListener("click", () => viewTaskDetails(task.id));
+
+    const taskInfo = document.createElement("div");
+    taskInfo.innerHTML = `
         <p><b>Title</b>: ${task.name}</p>
         <p><b>Description</b>: ${task.description}</p>
         <p><b>Date:</b> ${task.date}</p>
       `;
-  
-      const editButton = document.createElement('button');
-      editButton.innerText = 'Edit';
-      editButton.addEventListener('click', () => editTask(task.id));
-  
-      const deleteButton = document.createElement('button');
-      deleteButton.innerText = 'Delete';
-      deleteButton.addEventListener('click', () => deleteTask(task.id));
-  
-      taskDiv.appendChild(checkbox);
-      taskDiv.appendChild(taskInfo);
-      taskDiv.appendChild(editButton);
-      taskDiv.appendChild(deleteButton);
-  
-      if (task.completed) {
-        completedTasksDiv.appendChild(taskDiv);
-      } else {
-        incompletedTasksDiv.appendChild(taskDiv);
-      }
-    });
-  }
 
-  function toggleTaskCompletion(taskId) {
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    tasks = tasks.map(task => {
-      if (task.id === taskId) {
-        task.completed = !task.completed;
-      }
-      return task;
-    });
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    loadTasks();
-  }
-  
-  
-  function deleteTask(taskId) {
-    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    tasks = tasks.filter(task => task.id !== taskId);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    loadTasks();
-  }
-  
-  
-  function generateUniqueId() {
-    return "id" + Math.random().toString(16).slice(2)
-  }
-  
+    const editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.addEventListener("click", () => editTask(task.id));
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener("click", () => deleteTask(task.id));
+
+    taskDiv.appendChild(checkbox);
+    taskDiv.appendChild(taskNameDiv);
+    taskDiv.appendChild(taskInfo);
+    taskDiv.appendChild(editButton);
+    taskDiv.appendChild(deleteButton);
+
+    if (task.completed) {
+      completedTasksDiv.appendChild(taskDiv);
+    } else {
+      incompletedTasksDiv.appendChild(taskDiv);
+    }
+  });
+}
+
+function toggleTaskCompletion(taskId) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks = tasks.map((task) => {
+    if (task.id === taskId) {
+      task.completed = !task.completed;
+    }
+    return task;
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  loadTasks();
+}
+console.log(localStorage);
+
+function deleteTask(taskId) {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks = tasks.filter((task) => task.id !== taskId);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  loadTasks();
+}
+
+function generateUniqueId() {
+  return "id" + Math.random().toString(16).slice(2);
+}
+
 function viewTaskDetails(taskId) {
   const detailsUrl = `/details/?id=${taskId}`;
   window.location.assign(detailsUrl);
@@ -160,12 +164,8 @@ function filterTasks() {
   loadTasks(false, false, status);
 } 
 
-
-
-  
-  
-  
-  
-  
-  
-  
+document
+  .getElementById("taskDescription")
+  .addEventListener("input", function () {
+    this.style.textAlign = "center"; 
+  });
